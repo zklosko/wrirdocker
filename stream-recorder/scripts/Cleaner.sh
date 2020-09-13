268f5376a020a9e7c105dbdfc6766a84e1262367
Zachary Klosko <kloskoz@vcu.edu>
2020-09-13 08:20:48 -0400

    Selfgen selfsigned SSL cert works for webdav

--- stream-recorder/scripts/Cleaner.sh
+++ stream-recorder/scripts/Cleaner.sh
@@ -3,7 +3,7 @@
 sDays=75
 echo Starting $0 $(date)
 
-sOldFiles=$(find /scripts/raw/* -ctime +${sDays})  # refactered line
+sOldFiles=$(find ./raw/* -ctime +${sDays})  # refactered line
 [[ -n "${sOldFiles}" ]] && rm ${sOldFiles}
 
 echo Ending $0 $(date)
