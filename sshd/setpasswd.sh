6245979f2fa2486ef5da53bb4313b3e5c898c254
Zachary Klosko <kloskoz@vcu.edu>
2020-09-25 14:58:28 -0400

    Trying docker compose again

--- sshd/setpasswd.sh
+++ sshd/setpasswd.sh
@@ -8,3 +8,5 @@ echo 'zackaklosko:BxHi2wZxBn>D8K;Q' | chpasswd
 
 # Or if you do pre-hash the password remove the line above and uncomment the line below.
 # echo "user1:passwordhashgoeshere" | chpasswd --encrypted
+
+/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
